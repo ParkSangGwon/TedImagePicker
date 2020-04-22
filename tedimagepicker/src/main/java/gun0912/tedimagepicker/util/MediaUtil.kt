2cfc9f0be3dbcefd45e5728174290de0cd3d04f7
ted-prnd <ted@prnd.co.kr>
2020-04-22 16:01:54 +0900

    Using enum's feild / Adjust scope storage camera intent uri

--- tedimagepicker/src/main/java/gun0912/tedimagepicker/util/MediaUtil.kt
+++ tedimagepicker/src/main/java/gun0912/tedimagepicker/util/MediaUtil.kt
@@ -1,5 +1,6 @@
 package gun0912.tedimagepicker.util
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -7,10 +8,8 @@ import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore
-import androidx.core.content.FileProvider
 import gun0912.tedimagepicker.builder.type.MediaType
 import io.reactivex.Completable
-import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -27,17 +26,11 @@ internal class MediaUtil {
                 MediaType.VIDEO -> Intent(MediaStore.ACTION_VIDEO_CAPTURE)
             }
 
-            val mediaFile = getMediaFile(mediaType, savedDirectoryName)
+            val mediaUri = getMediaUri(context, mediaType, savedDirectoryName)
 
             if (cameraIntent.resolveActivity(context.packageManager) == null) {
                 throw PackageManager.NameNotFoundException("Can not start Camera")
             }
-
-            val photoURI = FileProvider.getUriForFile(
-                context,
-                context.applicationContext.packageName + ".provider",
-                mediaFile
-            )
 
             val resolvedIntentActivities = context.packageManager
                 .queryIntentActivities(cameraIntent, PackageManager.MATCH_DEFAULT_ONLY)
@@ -45,36 +38,38 @@ internal class MediaUtil {
                 val packageName = resolvedIntentInfo.activityInfo.packageName
                 context.grantUriPermission(
                     packageName,
-                    photoURI,
+                    mediaUri,
                     Intent.FLAG_GRANT_WRITE_URI_PERMISSION or Intent.FLAG_GRANT_READ_URI_PERMISSION
                 )
             }
 
-            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mediaUri)
 
-            return Pair(cameraIntent, Uri.fromFile(mediaFile))
+            return Pair(cameraIntent, mediaUri)
         }
 
-        private fun getMediaFile(mediaType: MediaType, savedDirectoryName: String?): File {
+        private fun getMediaUri(
+            context: Context,
+            mediaType: MediaType,
+            savedDirectoryName: String?
+        ): Uri {
             val timeStamp =
                 SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).format(Date())
             val fileName = "${mediaType}_$timeStamp"
-            val directoryName = savedDirectoryName ?: when (mediaType) {
-                MediaType.IMAGE -> Environment.DIRECTORY_PICTURES
-                MediaType.VIDEO -> Environment.DIRECTORY_MOVIES
-            }
+            val directoryName = savedDirectoryName ?: mediaType.savedDirectoryName
             val directory = Environment.getExternalStoragePublicDirectory(directoryName)
 
             if (!directory.exists()) {
                 directory.mkdir()
             }
 
-            val fileSuffix = when (mediaType) {
-                MediaType.IMAGE -> ".jpg"
-                MediaType.VIDEO -> ".mp4"
+            val contentValues = ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, fileName + mediaType.fileSuffix)
+                put(MediaStore.MediaColumns.MIME_TYPE, mediaType.mimeType)
+                put(MediaStore.MediaColumns.RELATIVE_PATH, directoryName)
             }
 
-            return File.createTempFile(fileName, fileSuffix, directory)
+            return context.contentResolver.insert(mediaType.externalContentUri, contentValues)!!
         }
 
         fun scanMedia(context: Context, uri: Uri): Completable {
