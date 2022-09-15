eeb693c8ba8fe6a46d4bf6b39f2147d9fca244c1
ted-prnd <ted@prnd.co.kr>
2022-09-15 15:02:35 +0900

    Update LeakCanary version

--- app/src/main/java/gun0912/tedimagepicker/sample/MyApplication.kt
+++ app/src/main/java/gun0912/tedimagepicker/sample/MyApplication.kt
@@ -3,21 +3,10 @@ package gun0912.tedimagepicker.sample
 
 import androidx.multidex.MultiDexApplication
 
-import com.squareup.leakcanary.LeakCanary
-
 class MyApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        setLeakCanary()
     }
 
-    private fun setLeakCanary() {
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return
-        }
-        LeakCanary.install(this)
-    }
 }
