e77050bd88eecff6f84cb30fb3defbabbed97428
ted-prnd <ted@prnd.co.kr>
2023-01-04 12:03:31 +0900

    extract function to getAllMediaList()

--- tedimagepicker/src/main/java/gun0912/tedimagepicker/util/GalleryUtil.kt
+++ tedimagepicker/src/main/java/gun0912/tedimagepicker/util/GalleryUtil.kt
@@ -12,6 +12,7 @@ import gun0912.tedimagepicker.model.Album
 import gun0912.tedimagepicker.model.Media
 import io.reactivex.Single
 import java.io.File
+import java.util.Collections.addAll
 
 internal class GalleryUtil {
     companion object {
@@ -25,76 +26,72 @@ internal class GalleryUtil {
         internal fun getMedia(context: Context, mediaType: MediaType): Single<List<Album>> {
             return Single.create { emitter ->
                 try {
-
-                    val uri: Uri
-
-                    when (mediaType) {
-                        MediaType.IMAGE -> {
-                            uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-                            albumName = MediaStore.Images.Media.BUCKET_DISPLAY_NAME
-                        }
-                        MediaType.VIDEO -> {
-                            uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
-                            albumName = MediaStore.Video.Media.BUCKET_DISPLAY_NAME
-                        }
-                    }
-
-                    val sortOrder = "$INDEX_DATE_ADDED DESC"
-
-                    val projection = arrayOf(
-                        INDEX_MEDIA_ID,
-                        INDEX_MEDIA_URI,
-                        albumName,
-                        INDEX_DATE_ADDED
-                    )
-                    val selection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                        MediaStore.Images.Media.SIZE + " > 0"
-                    } else {
-                        null
-                    }
-                    val cursor =
-                        context.contentResolver.query(uri, projection, selection, null, sortOrder)
-                    val albumList: List<Album> = cursor?.let {
-
-                        val totalMediaList =
-                            generateSequence { if (cursor.moveToNext()) cursor else null }
-                                .map { getMedia(it, mediaType) }
-                                .filterNotNull()
-                                .toList()
-
-                        val albumList: List<Album> = totalMediaList
-                            .groupBy { media: Media -> media.albumName }
-                            .toSortedMap { albumName1: String, albumName2: String ->
-                                if (albumName2 == "Camera") {
-                                    1
-                                } else {
-                                    albumName1.compareTo(albumName2, true)
-                                }
+                    val totalMediaList = getAllMediaList(context, mediaType)
+                    val albumList: List<Album> = totalMediaList
+                        .groupBy { media: Media -> media.albumName }
+                        .toSortedMap { albumName1: String, albumName2: String ->
+                            if (albumName2 == "Camera") {
+                                1
+                            } else {
+                                albumName1.compareTo(albumName2, true)
                             }
-                            .map { entry -> getAlbum(entry) }
-                            .toList()
-
-
-                        val totalAlbum = totalMediaList.run {
-                            val albumName = context.getString(R.string.ted_image_picker_album_all)
-                            Album(
-                                albumName,
-                                getOrElse(0) { Media(albumName, Uri.EMPTY, 0) }.uri,
-                                this
-                            )
                         }
+                        .map { entry -> getAlbum(entry) }
+                        .toList()
 
-                        mutableListOf(totalAlbum).apply {
-                            addAll(albumList)
-                        }
-                    } ?: emptyList()
 
-                    cursor?.close()
-                    emitter.onSuccess(albumList)
+                    val totalAlbum = totalMediaList.run {
+                        val albumName = context.getString(R.string.ted_image_picker_album_all)
+                        Album(
+                            albumName,
+                            getOrElse(0) { Media(albumName, Uri.EMPTY, 0) }.uri,
+                            this
+                        )
+                    }
+
+                    val result = mutableListOf(totalAlbum).apply { addAll(albumList) }
+                    emitter.onSuccess(result)
                 } catch (exception: Exception) {
                     emitter.onError(exception)
                 }
+            }
+        }
 
+        private fun getAllMediaList(context: Context, mediaType: MediaType): List<Media> {
+            val uri: Uri
+            when (mediaType) {
+                MediaType.IMAGE -> {
+                    uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    albumName = MediaStore.Images.Media.BUCKET_DISPLAY_NAME
+                }
+                MediaType.VIDEO -> {
+                    uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                    albumName = MediaStore.Video.Media.BUCKET_DISPLAY_NAME
+                }
+            }
+
+            val sortOrder = "$INDEX_DATE_ADDED DESC"
+
+            val projection = arrayOf(
+                INDEX_MEDIA_ID,
+                INDEX_MEDIA_URI,
+                albumName,
+                INDEX_DATE_ADDED
+            )
+            val selection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                MediaStore.Images.Media.SIZE + " > 0"
+            } else {
+                null
+            }
+            val cursor =
+                context.contentResolver.query(uri, projection, selection, null, sortOrder)
+                    ?: return emptyList()
+
+            cursor.use {
+                return generateSequence { if (cursor.moveToNext()) cursor else null }
+                    .map { getMedia(it, mediaType) }
+                    .filterNotNull()
+                    .toList()
             }
         }
 
