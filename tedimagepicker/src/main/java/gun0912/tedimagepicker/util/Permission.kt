01213cf00adf048b603655e2a46b0edbaae58072
ted-prnd <gun0912@naver.com>
2024-01-26 08:48:50 +0900

    Add useful permission value property

---
+++ tedimagepicker/src/main/java/gun0912/tedimagepicker/util/Permission.kt
@@ -0,0 +1,15 @@
+package gun0912.tedimagepicker.util
+
+import android.Manifest
+import android.os.Build
+import com.gun0912.tedpermission.TedPermissionUtil
+import gun0912.tedimagepicker.builder.type.MediaType
+
+
+internal val isPartialAccessGranted: Boolean
+    get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE
+        && TedPermissionUtil.isGranted(Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED)
+
+
+internal val MediaType.isFullOrPartialAccessGranted: Boolean
+    get() = TedPermissionUtil.isGranted(*permissions) || isPartialAccessGranted
